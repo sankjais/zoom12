cb11a68336b3782bceeecf186e0d59a8932c0f15
sanket <sanket@SANKET-PC.isynergy.com>
2018-05-09 15:37:59 +0530

    @EOD-1 9/05/2018

--- zoom12/src/smoke/Login.java
+++ zoom12/src/smoke/Login.java
@@ -1,5 +1,23 @@
 package smoke;
 
+import testsuit.TestData;
+
 public class Login {
+
+	public static void main(String args[]) {
+
+	TestData data= new TestData();
+	data=data.creatTestData();
+	for (int r = 1; r < TestData.row; r++) {
+		for (int c = 0; c < TestData.col; c++) {
+
+			//tdata[r][c] = exceldata.getCellDataasstring(r, c);
+			System.out.print(  "  ");
+		}
+		System.out.println("\n****************");
+	}
+	
+	
+	}
 
 }
